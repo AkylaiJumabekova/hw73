c907330973ca589d870b48edfc32b1b65ba31111
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-08-02 14:54:30 +0600

    added second tasks logic

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
 const app = express();
 const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = 'password';
 
 app.get('/:message', (req, res) => {
     return res.send(req.params.message);
+});
+
+app.get('/encode/:text', (req, res) => {
+    const encodedText = Vigenere.Cipher(password).crypt(req.params.text);
+    return res.send(encodedText);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const decodedText = Vigenere.Decipher(password).crypt(req.params.text);
+    return res.send(decodedText);
 });
 
 app.listen(port, () => {
